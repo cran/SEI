20e4a5679a8d542fdcc9e0617b1e6d605fd3cd36
Sam Allen <sam.allen@unibe.ch>
2024-01-26 16:20:02 +0000

    version 0.1.1

--- inst/doc/SEI_vignette.R
+++ inst/doc/SEI_vignette.R
@@ -36,10 +36,6 @@ grid.arrange(plot_raw, plot_std, nrow = 1)
 data("data_supply", package = "SEI")
 head(data_supply)
 
-## ----load_ic------------------------------------------------------------------
-data("data_ic2017", package = "SEI")
-head(data_ic2017)
-
 ## ----subset_germany-----------------------------------------------------------
 de_supply_h <- subset(data_supply, country == "Germany")
 de_supply_h <- xts::xts(de_supply_h$PWS, de_supply_h$date) # convert to xts
@@ -55,10 +51,10 @@ plot_d <- plot_sei(de_supply_d, lab = lab, title = "Daily")
 plot_w <- plot_sei(de_supply_w, lab = lab, title = "Weekly")
 grid.arrange(plot_h, plot_d, plot_w, nrow = 1)
 
-## ----calculate_index, warning=FALSE-------------------------------------------
+## ----calculate_index----------------------------------------------------------
 srepi_h <- std_index(de_supply_h)
 srepi_d <- std_index(de_supply_d)
-srepi_w <- std_index(de_supply_w)
+srepi_w <- std_index(de_supply_w, dist = "kde")
 
 ## ----use_rescale--------------------------------------------------------------
 z <- std_index(de_supply_h, rescale = "days")
@@ -66,9 +62,10 @@ all.equal(srepi_d, z)
 
 ## ----plot_sei_ts, dev='pdf', fig.width=9, fig.height=3, fig.align="center", out.width = "\\linewidth"----
 lab <- "SREPI"
-plot_h <- plot_sei(srepi_h, lab = lab, title = "Hourly")
-plot_d <- plot_sei(srepi_d, lab = lab, title = "Daily")
-plot_w <- plot_sei(srepi_w, lab = lab, title = "Weekly")
+ylims <- c(-3, 3)
+plot_h <- plot_sei(srepi_h, lab = lab, ylims = ylims, title = "Hourly")
+plot_d <- plot_sei(srepi_d, lab = lab, ylims = ylims, title = "Daily")
+plot_w <- plot_sei(srepi_w, lab = lab, ylims = ylims, title = "Weekly")
 grid.arrange(plot_h, plot_d, plot_w, nrow = 1)
 
 ## ----plot_dist, dev='pdf', fig.width=9, fig.height=3, fig.align="center", out.width = "\\linewidth"----
@@ -91,7 +88,7 @@ thresholds <- -qnorm(c(0.9, 0.95, 0.975)) # -1.28, -1.64, -1.96
 drought_df <- get_drought(srepi_d, thresholds, exceed = F)
 
 ## ----calculate event frequency------------------------------------------------
-num_ev <- sapply(0:3, function(i) sum(drought_df$ins == i))
+num_ev <- table(drought_df$ins)
 names(num_ev) <- c("None", "Moderate", "Severe", "Extreme")
 print(num_ev)
 
@@ -100,4 +97,76 @@ table(drought_df$dur[drought_df$dur > 0])
 
 ## ----calculate event magnitude------------------------------------------------
 mean(drought_df$mag[drought_df$mag != 0])
+
+## ----load_wind_data-----------------------------------------------------------
+data("data_wind_de", package = "SEI")
+data_wind_de <- subset(data_wind_de, format(date, "%Y") >= 2017)
+head(data_wind_de)
+
+## ----rescale_wind, echo=FALSE-------------------------------------------------
+de_wind_d <- xts::xts(data_wind_de$wsmean, data_wind_de$date) # convert to xts
+de_wind_w <- xts::apply.weekly(de_wind_d, FUN = "mean")       # weekly data
+de_wind_m <- xts::apply.monthly(de_wind_d, FUN = "mean")      # monthly data
+
+## ----plot_raw_ts_wind, dev='pdf', fig.width=12, fig.height=4, fig.align="center", out.width = "\\linewidth"----
+lab <- "Wind speed (m/s)"
+ylims <- c(0, 8)
+plot_ws_d <- plot_sei(de_wind_d, lab = lab, ylims = ylims, title = "Daily")
+plot_ws_w <- plot_sei(de_wind_w, lab = lab, ylims = ylims, title = "Weekly")
+plot_ws_m <- plot_sei(de_wind_m, lab = lab, ylims = ylims, title = "Monthly")
+grid.arrange(plot_ws_d, plot_ws_w, plot_ws_m, nrow = 1)
+
+## ----fit_ws_dists-------------------------------------------------------------
+out_gamma <- fit_dist(data_wind_de$wsmean, dist = "gamma")
+out_lnorm <- fit_dist(data_wind_de$wsmean, dist = "lnorm")
+out_weibull <- fit_dist(data_wind_de$wsmean, dist = "weibull")
+
+## ----aic----------------------------------------------------------------------
+aic_vec <- c(out_gamma$fit_props['aic'],
+             out_lnorm$fit_props['aic'],
+             out_weibull$fit_props['aic'])
+names(aic_vec) <- c("Gamma", "Log-normal", "Weibull")
+print(aic_vec)
+
+## ----ksp----------------------------------------------------------------------
+ksp_vec <- c(out_gamma$fit_props['ks_pval'],
+             out_lnorm$fit_props['ks_pval'],
+             out_weibull$fit_props['ks_pval'])
+names(ksp_vec) <- c("Gamma", "Log-normal", "Weibull")
+print(round(ksp_vec, 4))
+
+## ----ws_plot_dist_gam---------------------------------------------------------
+x <- seq(0, 9, length.out = length(de_wind_d))
+xlab <- "Wind speed (m/s)"
+pars_gam <- out_gamma$params
+
+plt_gam <- plot_sei(de_wind_d, type = "hist", lab = xlab, title = "Gamma") +
+  geom_line(aes(x = x, y = dgamma(x, pars_gam[1], pars_gam[2])), col = "blue")
+
+## ----ws_plot_dist, echo=FALSE, dev='pdf', fig.width=9, fig.height=3, fig.align="center", out.width = "\\linewidth"----
+# Log-normal distribution
+plt_lnorm <- plot_sei(de_wind_d, type = "hist", lab = xlab, title = "Log-normal")
+plt_lnorm <- plt_lnorm + geom_line(aes(x = x, y = dlnorm(x, out_lnorm$params[1], out_lnorm$params[2])), col = "blue")
+
+# Weibull distribution
+plt_weib <- plot_sei(de_wind_d, type = "hist", lab = xlab, title = "Weibull")
+plt_weib <- plt_weib + geom_line(aes(x = x, y = dweibull(x, out_weibull$params[1], out_weibull$params[2])), col = "blue")
+
+grid.arrange(plt_gam, plt_lnorm, plt_weib, nrow = 1)
+
+## ----get_std_indices----------------------------------------------------------
+sei_ws_gam <- std_index(de_wind_d, dist = "gamma")
+sei_ws_lnorm <- std_index(de_wind_d, dist = "lnorm")
+sei_ws_weib <- std_index(de_wind_d, dist = "weibull")
+
+## ----ws_ind_plot_dist, echo=FALSE, dev='pdf', fig.width=9, fig.height=3, fig.align="center", out.width = "\\linewidth"----
+x <- seq(-3.5, 3.5, length.out = length(sei_ws_gam))
+xlab <- "SWSI"
+plt_gam <- plot_sei(sei_ws_gam, type = "hist", lab = xlab, title = "Gamma") +
+  geom_line(aes(x = x, y = dnorm(x)), col = "blue")
+plt_lnorm <- plot_sei(sei_ws_lnorm, type = "hist", lab = xlab, title = "Log-normal") +
+  geom_line(aes(x = x, y = dnorm(x)), col = "blue")
+plt_weib <- plot_sei(sei_ws_weib, type = "hist", lab = xlab, title = "Weibull") +
+  geom_line(aes(x = x, y = dnorm(x)), col = "blue")
+grid.arrange(plt_gam, plt_lnorm, plt_weib, nrow = 1)
 
---
+++ vignettes/RJ/motivation-letter/motivation-letter.md
@@ -0,0 +1,43 @@
+---
+output: pdf_document
+fontsize: 12pt
+---
+
+\thispagestyle{empty}
+\today
+
+Dear editor, 
+	
+\bigskip
+	
+I hereby submit the research article titled ``Calculating standardised indices using SEI'' for publication in the R Journal. This manuscript is not in submission elsewhere, and the authors have no conflicts of interest to disclose.
+	
+\bigskip
+	
+This article is a vignette for the \verb!SEI! package in \verb!R!, which provides the functionality to calculate standardised indices. Standardised indices are projections of variables onto an interpretable and probabilistically meaningful scale, making them ideal for risk management and decision making. They have found applications in various fields, and are ubiquitous in the context of hydrological drought analysis. Existing software packages lack flexibility when computing standardised indices, typically restricting attention to only a few standardised climate indices. The \verb!SEI! package remedies this by providing comprehensive functionality that can be applied in much greater generality, facilitating applications of standardised variables in any domain.
+	
+\bigskip
+	
+The article motivates the use of standardised indices, thoroughly describes their theoretical background, and demonstrates the functionality of the SEI package with several examples. We discuss existing software packages and their limitations, list possible implementation options, and provide advice regarding which methods are most suitable in different circumstances. The article then illustrates how the software can be implemented in practice by applying it in two highly relevant applications in the fields of meteorology and renewable energy. The article therefore greatly facilitates the implementation of standardised indices in practical applications, rendering the manuscript suitable for publication in this journal.
+		
+\bigskip
+	 
+Material to replicate the results in this manuscript has been included in the submission.
+
+\bigskip
+	
+Thank you for considering this manuscript, I look forward to hearing your response. Do not hesitate to contact me if you have any queries.
+	
+\bigskip
+	
+Sincerely, 
+	
+\bigskip
+	
+Sam Allen
+    
+
+Institute of Mathetmatical Statistics and Actuarial Science  
+University of Bern  
+Bern, Switzerland  
+sam.allen@unibe.ch
